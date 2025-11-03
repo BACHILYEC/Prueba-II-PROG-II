89c52f6393c53af5c160d2db24deb266daee9997
Ismael Heredia <ismael.heredia@epn.edu.ec>
2025-11-03 00:19:28 -0500

    comp

--- src/pkInventario/pkReinoViviente/pkVegetal/PlantaConFlor/ReinaClaudia.java
+++ src/pkInventario/pkReinoViviente/pkVegetal/PlantaConFlor/ReinaClaudia.java
@@ -4,5 +4,6 @@ package pkInventario.pkReinoViviente.pkVegetal.PlantaConFlor;
 public class ReinaClaudia extends plantaConFruto {
   public ReinaClaudia(){
     super(true,true);
-}
+  }
+
 }
--- src/pkInventario/pkReinoViviente/pkVegetal/PlantaConFlor/plantaConFruto.java
+++ src/pkInventario/pkReinoViviente/pkVegetal/PlantaConFlor/plantaConFruto.java
@@ -3,7 +3,8 @@ package pkInventario.pkReinoViviente.pkVegetal.PlantaConFlor;
 import pkInventario.pkReinoViviente.ReinoVegetal;
 
 public abstract class plantaConFruto extends ReinoVegetal {
-public plantaConFruto(Boolean tieneFlor,Boolean tieneFruto){
-    super(tieneFlor,tieneFruto);
-}
+    public plantaConFruto(Boolean tieneFlor,Boolean tieneFruto){
+        super(tieneFlor,tieneFruto);
+    }
+    
 }
