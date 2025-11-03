a1c9762457ede3a4021f4070626e2573cfea2882
grandote8x8 <mathias@debian.debian>
2025-11-03 00:15:21 -0500

    Adicion de metodos de inicio para naranja

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
+import pkInventario.AppInventario;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        AppInventario inventario = new AppInventario();
+
     }
 }
--- src/pkInventario/AppInventario.java
+++ src/pkInventario/AppInventario.java
@@ -1,5 +1,11 @@
 package pkInventario;
 
+import pkInventario.pkReinoViviente.pkVegetal.PlantaConFlor.Naranja;
+
 public class AppInventario {
+
+    public void iniciarInventario(){
+        Naranja naranja = new Naranja();
+    }
 
 }
