a576f1a7b27567bb50540d4a4488b78388165eb2
KarliBoo <karli_cruz29@hotmail.com>
2025-11-05 00:17:57 -0500

    Agregando info de cliente

--- src/pkInventario/pkHumano/Cliente.java
+++ src/pkInventario/pkHumano/Cliente.java
@@ -1,17 +1,27 @@
-package pkInventario.pkHumano;
+public void visualizar(Naranja naranja, Durazno duraznito, Limon limoncito, ReinaClaudia reinita) {
+    String msg1 = "[estado] maduro";
+    String msg2 = "[estado] sin madurar";
+    System.out.println("---------------------");
+    if (naranja.getEstaMaduro()) {
+        System.out.println("* Naranaja - " + msg1);
+    } else {
+        System.out.println("* Naranaja - " + msg2);
+    }
+    if (duraznito.getEstaMaduro()) {
+        System.out.println("* Durazno - " + msg1);
+    } else {
+        System.out.println("* Durazno - " + msg2);
+    }
+    if (limoncito.getEstaMaduro()) {
+        System.out.println("* Limon - " + msg1);
+    } else {
+        System.out.println("* Limon - " + msg2);
+    }
 
-public class Cliente extends Persona{
-public Cliente(){
-    super();
-}
-
-public String retirar(){
-    String frase = "El cliente ha retirado las frutas que visualizó.";
-    return frase;
-}
-public String visualizar(){
-    String frase = "El cliente revisó las frutas que necesita.";
-    return frase;
-}
-
+    if (reinita.getEstaMaduro()) {
+        System.out.println("* Reina Claudia - " + msg1);
+    } else {
+        System.out.println("* Reinita Claudia - " + msg2);
+    }
+    System.out.println("---------------------");
 }
--- src/pkInventario/pkReinoViviente/pkVegetal/PlantaConFlor/Limon.java
+++ src/pkInventario/pkReinoViviente/pkVegetal/PlantaConFlor/Limon.java
@@ -1,7 +1,5 @@
 package pkInventario.pkReinoViviente.pkVegetal.PlantaConFlor;
 
-import ReinoVegetal.PlantaConFlor.ConFruto.plantaConFruto;
-
 public class Limon extends plantaConFruto{
     public Limon(){
         super(true,true);
